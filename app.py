6f4d6a9ac546b85bb6febc66d80bdb27e3e44827
Klismann Saavedra <klismannsaavedra@gmail.com>
2026-07-09 22:24:21 -0500

    Fix comparison operators in modulo selection

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.sidebar.image("DMC.png")
 
 modulo=st.side.selectbox("Elija un modulito",["Modulito Listas","Modulito Array","Modulito Funciones"])
 
-if modulo="Modulito Listas":
+if modulo=="Modulito Listas":
 
   valor_inicial = st.number_input("Ingrese el valor inicial",value=0)
   valor_final= st.number_input("Ingrese el valor final",value=1)
@@ -15,7 +15,7 @@ if modulo="Modulito Listas":
   lista_numerica=list(range(valor_inicial,valor_final))
   
   st.write(lista_numerica)
-elif modulo="Modulito Array"
+elif modulo=="Modulito Array"
     st.write("Estas en el modulito de arreglos")
     
 
