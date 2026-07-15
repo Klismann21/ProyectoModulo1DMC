e17af6ee67841fc06bd137bab73952bb2a2ae21d
Klismann Saavedra <klismannsaavedra@gmail.com>
2026-07-14 22:22:49 -0500

    Integrate loan calculation feature in app.py
    Added loan calculation inputs and functionality using libreria_funciones.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import streamlit as st
+import libreria_funciones as lf
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
@@ -31,4 +32,10 @@ elif modulo=="Modulito Array":
     
 else:
   st.write("Estas en el modulo de funciones")
-  
+
+  principal=st.number_input("Monto de Prestamo",value=0)
+  tasa_anual=st.number_input("Tasa anual en decimal",value=0.10)
+  anios=st.number_input("Ingrese el numero de años del prestamo",value=1)
+  pagos_por_anio=st.number_input("Ingrese la cantidad de veces a pagar por año",value=12)
+  cuota= lf.cuotaprestamo(principal,tasa_anual,pagos_por_anio)
+  st.write("El valor de las cuotas mensuales es :",cuota)
